4a83a97a374d105cc5e88dbf00156688018a180f
Basant Tamang <basanttamang8@gmail.com>
2021-06-28 11:00:02 +0545

    first commit in github

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
